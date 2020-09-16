c00c4d311875c3a5297a0c6b09f93c7f5230e60a
autowaller <malfunctioned@protonmail.com>
2020-09-16 16:52:44 +0300

    281A Solution

---
+++ Codeforces/Problemset/A-Sets/281A - Word Capitalization.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+    string word;
+    cin >> word;
+
+    if(word[0]>96) {
+        word[0] -= 32;
+    }
+
+    cout << word << endl;
+
+    return 0;
+}
